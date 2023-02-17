d6f4190f262a2dfab904ebf488286e3d41f71d79
Tim Medcalf <timbmedcalf@gmail.com>
2023-02-17 10:54:33 +0000

    removed faulty config line

--- lua/tim/plugins/lsp/null-ls.lua
+++ lua/tim/plugins/lsp/null-ls.lua
@@ -17,7 +17,6 @@ null_ls.setup({
   sources = {
     --  to disable file types use
     --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-    formatting.completion.spell,
     formatting.prettier, -- js/ts formatter
     formatting.stylua, -- lua formatter
     diagnostics.eslint_d.with({ -- js/ts linter
